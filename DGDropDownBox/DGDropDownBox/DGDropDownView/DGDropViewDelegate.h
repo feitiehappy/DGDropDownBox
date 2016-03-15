7b1be7904d7053236bff3dd11c90a3ba41fbaacf
yandeguang <754199572@qq.com>
2016-03-15 18:43:53 +0800

    增加浮层、midview、及按钮

---
+++ DGDropDownBox/DGDropDownBox/DGDropDownView/DGDropViewDelegate.h
@@ -0,0 +1,72 @@
+//
+//  DGDropViewDelegate.h
+//  DGDropDownBox
+//
+//  Created by FSLB on 16/3/15.
+//  Copyright © 2016年 FSLB. All rights reserved.
+//
+
+#ifndef DGDropViewDelegate_h
+#define DGDropViewDelegate_h
+
+#define SCREEN_W [UIScreen mainScreen].bounds.size.width
+#define SCREEN_H [UIScreen mainScreen].bounds.size.height
+
+typedef enum : NSUInteger {
+    DROP_DIRECTION_UP,
+    DROP_DIRECTION_DOWN
+} DROP_DIRECTION;
+
+typedef enum : NSUInteger {
+    DROPTEXT   = 0,     //仅文字
+    DROPTEXTANDIMAGR    //图片加文字
+} DROPDOWNVIEWTYPE;
+
+
+@protocol dropViewBoxDelegate <NSObject>
+
+
+
+@end
+
+@protocol dropViewBoxDataSource <NSObject>
+/**
+ *  @author Yan deguang, 16-03-15 15:03:06
+ *
+ *  返回item个数
+ *
+ *  @return item.count
+ */
+- (NSInteger) numberDropViewItems;
+
+/**
+ *  @author Yan deguang, 16-03-15 16:03:10
+ *
+ *  返回每个item的size
+ *
+ *  @param dropView dropView
+ *  @param index    第几个，从0开始
+ *
+ *  @return
+ */
+- (CGSize) dropView:(UIView *)dropView itemindex:(NSInteger)index;
+
+- (NSString *) dropView:(UIView *)dropView itemTitleText:(NSInteger)index;//每个itemTitle
+@optional
+/**
+ *  @author Yan deguang, 16-03-15 16:03:03
+ *
+ *  返回是在上面弹出还是下面弹出，默认为下面
+ *
+ *  @return 
+ */
+
+- (DROP_DIRECTION) dropViewOrientation;
+- (NSString *) dropView:(UIView *)dropView itemImageText:(NSInteger)index;//每个itemImage
+
+
+@end
+
+
+
+#endif /* DGDropViewDelegate_h */
--- DGDropDownBox/DGDropDownBox/ViewController.m
+++ DGDropDownBox/DGDropDownBox/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "DGDropBoxView.h"
 
-@interface ViewController ()
+@interface ViewController ()<dropViewBoxDataSource,dropViewBoxDelegate>
 
+@property (nonatomic, strong) DGDropBoxView *dropView;
+
+@property (nonatomic, strong) NSMutableArray *dataArray;
 @end
 
 @implementation ViewController
@@ -17,6 +21,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)btnCLick:(id)sender {
+    _dropView = [DGDropBoxView dropViewWithLocationView:(UIButton *)sender dropDownViewType:DROPTEXT delegate:self];
+    [self.view addSubview:_dropView];
+}
+
+- (NSMutableArray *)dataArray{
+    if(!_dataArray){
+        _dataArray = [[NSMutableArray alloc] initWithObjects:@"选项一",@"选项二",@"选项三",@"选项四", nil];
+    }
+    return _dataArray;
+}
+
+- (NSInteger)numberDropViewItems{
+    return self.dataArray.count;
+}
+
+- (CGSize) dropView:(UIView *)dropView itemindex:(NSInteger)index{
+    return CGSizeMake(100, 40);
+}
+
+- (NSString *)dropView:(UIView *)dropView itemTitleText:(NSInteger)index{
+    return self.dataArray[index];
 }
 
 - (void)didReceiveMemoryWarning {
